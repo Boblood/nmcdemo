fc6d82ec4c5c4f7e29ee24d6b6820be09fc267cb
Emil O'Brien <ejobrien714@gmail.com>
2016-02-08 08:22:40 -0500

    First

---
+++ nmcdemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="nmcdemo.MvcApplication" Language="C#" %>
---
+++ nmcdemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(nmcdemo.Startup))]
+namespace nmcdemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
